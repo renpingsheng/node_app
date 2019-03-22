5bd4d6e79a2fd7cd1f692a4532a550f4f53b966b
renpingsheng <renpingsheng@canway.net>
2019-03-22 19:22:18 +0800

    开发登录接口,用户通过认证返回token

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI:"mongodb://192.168.86.17:27017/node_app"
+    mongoURI:"mongodb://192.168.86.17:27017/node_app",
+    secretOrKey:"secret"
 }
--- routes/api/users.js
+++ routes/api/users.js
@@ -6,28 +6,30 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
 const gravatar = require("gravatar");
+const jwt = require("jsonwebtoken");
 const router = express.Router();
 
 const User = require("../../models/User");
+const keys = require("../../config/keys");
 
 // $route GET api/users/test
 // @desc:返回的json数据
 // @access: public
 router.get("/test", (req, res) => {
     res.json({msg: "login works"})
-})
+});
 
 
 // $route POST api/users/register
 // @desc:返回的json数据
 // @access: public
-router.post('/register', (req, res) => {
+router.post("/register", (req, res) => {
     // 查询数据库中是否拥有邮箱
-    User.findOne({ email: req.body.email }).then(user => {
+    User.findOne({email: req.body.email}).then(user => {
         if (user) {
-            return res.status(400).json({email:'邮箱已被注册!'});
+            return res.status(400).json({email: '邮箱已被注册!'});
         } else {
-            const avatar = gravatar.url(req.body.email,{s:"200",r:"pg",d:"mm"});
+            const avatar = gravatar.url(req.body.email, {s: "200", r: "pg", d: "mm"});
             const newUser = new User({
                 name: req.body.name,
                 email: req.body.email,
@@ -35,7 +37,7 @@ router.post('/register', (req, res) => {
                 password: req.body.password
             });
 
-            bcrypt.genSalt(10, function(err, salt) {
+            bcrypt.genSalt(10, function (err, salt) {
                 bcrypt.hash(newUser.password, salt, (err, hash) => {
                     if (err) throw err;
                     newUser.password = hash;
@@ -47,6 +49,37 @@ router.post('/register', (req, res) => {
             });
         }
     });
+});
+
+
+// $route POST api/users/login
+// @desc:返回用户登录的token jwt passport
+// @access: public
+router.post("/login", (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    // 查询数据库
+    User.findOne({email}).then(user => {
+        if (!user) {
+            return res.status(404).json({email: "用户不存在!!!"})
+        }
+        // 密码匹配
+        bcrypt.compare(password, user.password).then(isMatch => {
+            if (isMatch) {
+                const rule = {id:user.id,name:user.name};
+                jwt.sign(rule,keys.secretOrKey,{expiresIn:3600},(err,token) =>{
+                    if(err) throw err;
+                    res.json({
+                        success:true,
+                        token:keys + token
+                    })
+                })
+            } else {
+                return res.status(400).json({password: "密码错误!!"})
+            }
+        })
+    })
 });
 
 module.exports = router;
