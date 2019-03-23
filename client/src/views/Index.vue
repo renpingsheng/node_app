d240741c57be383510e83f6eef1d603608cc98de
renpingsheng <renpingsheng@canway.net>
2019-03-23 14:03:07 +0800

    前端页面准备

---
+++ client/src/views/Index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="index">初始化页面</div>
+</template>
+
+<script>
+    export default {
+        name: "index",
+        components:{}
+    }
+</script>
+
+<style scoped>
+
+</style>
