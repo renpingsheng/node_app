af51050850a249cff29a19f7f79676802a85bb81
renpingsheng <renpingsheng@canway.net>
2019-03-23 16:15:15 +0800

    修改router.js文件,完成路由守卫

--- client/src/router.js
+++ client/src/router.js
@@ -7,7 +7,7 @@ import Login from './views/Login.vue'
 
 Vue.use(Router);
 
-export default new Router({
+const router = new Router({
     mode: 'history',
     base: process.env.BASE_URL,
     routes: [
@@ -36,4 +36,20 @@ export default new Router({
             component: NotFound
         }
     ]
+});
+
+
+//路由守卫
+router.beforeEach((to, from, next) => {
+    const isLogin = localStorage.eleToken ? true : false;
+    if (to.path == '/login' || to.path == '/register') {
+        next();
+    } else {
+        console.log("isLogin: " + isLogin);
+        isLogin ? next() : next('/login');
+    }
+
 })
+
+
+export default router;
